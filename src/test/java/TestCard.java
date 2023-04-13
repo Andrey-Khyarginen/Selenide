264197e9e8d4608eb6b602416e89017ed0c34955
Andrey Kharginen <kut13@inbox.ru>
2023-04-13 22:11:42 +0400

    add .appveyor.yml

--- src/test/java/TestCard.java
+++ src/test/java/TestCard.java
@@ -1,2 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 public class TestCard {
+
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    @Test
+    void shouldSendForm() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Саратов");
+        String currentDate = generateDate(3);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(currentDate);
+        $("[data-test-id=name] input").setValue("Алеша Васин");
+        $("[data-test-id=phone] input").setValue("+9998887766");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $x("//*[@class='notification__content']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
+    }
 }
